52ea435142e500a72752f429f92bdc29f88a7dec
Alan deLevie <adelevie@gmail.com>
2014-06-26 16:46:36 -0400

    updated json response for quick answers controller

--- app/controllers/quick_answers_controller.rb
+++ app/controllers/quick_answers_controller.rb
@@ -3,45 +3,45 @@ class QuickAnswersController < ApplicationController
   
   def show
     article = QuickAnswer.friendly.find(params[:id])
-    unless article
+    
+    unless article || article.published?
       add_breadcrumb "(404) Page not found"
-      return render(:template => 'articles/missing') 
+      respond_to do |format|
+        format.html { return render template: 'articles/missing'  }
+        format.json { return render json: article }
+      end
     end
-
-    unless article.published?
-      add_breadcrumb "(404) Page not found"
-      return render(template: 'articles/missing') 
-    end
+    
+    add_breadcrumb(article.category.name, article.category) if article.category.present?
+    add_breadcrumb(article.title)
 
     article.delay.record_hit
 
     unless article.render_markdown
       content_html = article.content
-      render :show_html and return
+      respond_to do |format|
+        locals = {
+          article: article,
+          content_html: content_html
+        }
+        format.html { return render locals: locals }
+        format.json { return render json: json }
+      end
     end
 
-    add_breadcrumb(article.category.name, article.category) if article.category.present?
-    add_breadcrumb(article.title)
-
-    content_main =  article.md_to_html( :content_main )
-    content_main_extra = article.md_to_html( :content_main_extra )
-    content_need_to_know =  article.md_to_html( :content_need_to_know )
-    content_main_es =  article.md_to_html( :content_main_es )
-    content_main_cn =  article.md_to_html( :content_main_cn )
+    locals = {
+      article: article,
+      content_html: content_html,
+      content_main: article.md_to_html(:content_main),
+      content_main_extra:  article.md_to_html(:content_main_extra),
+      content_need_to_know: article.md_to_html(:content_need_to_know),
+      content_main_es: article.md_to_html(:content_main_es),
+      content_main_cn: article.md_to_html(:content_main_cn)
+    }
 
     respond_to do |format|
-      format.html do
-        render locals: {
-          article: article,
-          content_html: content_html,
-          content_main: content_main,
-          content_main_extra: content_main_extra,
-          content_need_to_know: content_need_to_know,
-          content_main_es: content_main_es,
-          content_main_cn: content_main_cn
-        }
-      end
-      format.json { render json: article }
+      format.html { render locals: locals }
+      format.json { render json: locals }
     end
   end
 end
