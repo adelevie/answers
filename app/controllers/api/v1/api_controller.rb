99e55b636af027f183854437ab188895b63f9fc7
Alan deLevie <adelevie@gmail.com>
2014-07-28 15:01:12 -0400

    prevent Rails exception when record not found

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,4 +1,11 @@
 class Api::V1::ApiController < ApplicationController
   acts_as_token_authentication_handler_for User, only: [:create, :update, :destroy]
   prepend_view_path "app/views/api/v1"
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  
+  private
+  
+    def record_not_found
+      render json: {message: '404 Not Found', status: 404}, status: 404
+    end
 end
