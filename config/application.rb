af906f49bdfab3179a01fa08b104e1da78286351
Alan deLevie <adelevie@gmail.com>
2014-07-28 15:22:36 -0400

    Cleaned up files not meant for this PR

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ module Answers
     # -- all .rb files in that directory are automatically loaded.
 
     config.autoload_paths += %W( #{config.root}/lib #{config.root}/lib/rails_nlp )
-    config.autoload_paths += %W( #{config.root}/lib/rails_nlp )
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
