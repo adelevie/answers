3de2dbac20d951e3ef2c30dfe1ec545b38f78d1d
Justin Grevich <justin@grevi.ch>
2014-08-04 23:22:02 -0700

    Generate secret files using encrypted data bags

--- kitchen/site-cookbooks/answers/attributes/default.rb
+++ kitchen/site-cookbooks/answers/attributes/default.rb
@@ -2,5 +2,28 @@
 # Cookbook Name:: answers
 # Attributes:: default
 #
-default['answers']['rails']['env']									= 'production'
-default['answers']['ruby']['version']								= '2.1.2'
+default['answers']['rails']['env']										= 'production'
+default['answers']['ruby']['version']									= '2.1.2'
+
+###########
+# env setup
+###########
+
+# app vars
+default['answers']['env']['background_image']					= '/assets/background_image.jpg'
+default['answers']['env']['city_name']								= 'answers'
+default['answers']['env']['site_title']								= 'answers platform'
+default['answers']['env']['url']											= 'http://example.gov'
+default['answers']['env']['email']										= 'contact@example.gov'
+default['answers']['env']['sytle_guide']							= 'http://example.gov'
+
+# 3rd party config + keys
+default['answers']['env']['coveralls_run_locally']		= false
+default['answers']['env']['coveralls_token']					= nil
+default['answers']['env']['google_analytics_key']			= nil
+default['answers']['env']['new_relic_key']						= nil
+default['answers']['env']['s3_bucket']								= nil
+default['answers']['env']['s3_key']										= nil
+default['answers']['env']['s3_secret']								= nil
+default['answers']['env']['secret_key_base']					= '29530da3e01ab7094b2d37ab76da69c96e14eb49bd60d769897dc556e7eb0e0cab7ec0f811c1f61f6512ddf074b7c1caa4cf7f26678ffeaac7ccf7e30288e18a'
+default['answers']['env']['secret_token']							= 'd6a6cd0c45e59a27e3ace0d7280a2c01dbeab7595620e3ac77378ac74ceae64d0ec17bfeefa3bff995853594bb93e0197bba23f947d11deb4d7f7baf890e5f07'
--- kitchen/site-cookbooks/answers/recipes/default.rb
+++ kitchen/site-cookbooks/answers/recipes/default.rb
@@ -1,11 +1,14 @@
-# install required libs for memcached and postgresql
-['libsasl2-dev', 'libpq-dev', 'ruby-dev'].each { |pkg| package pkg }
-
-# install ruby
+# include associated recipes
 include_recipe "rbenv::default"
 include_recipe "rbenv::ruby_build"
 include_recipe "chef-solo-search"
 
+
+# install required libs for memcached and postgresql
+['libsasl2-dev', 'libpq-dev', 'ruby-dev'].each { |pkg| package pkg }
+
+
+# setup db users using encrypted data bags
 db_config = Chef::EncryptedDataBagItem.load("config", "db")
 
 puts "Adding DB Users:"
@@ -18,16 +21,39 @@ db_config["users"].each do |db_user|
 end if db_config["users"]
 puts "\n\n"
 
+
+# install ruby
 rbenv_ruby node[:answers][:ruby][:version] do
 	global true
 end
+
 
 # install bundler and passegner systemwide
 ['bundler', 'passenger'].each do |cmd|
 	rbenv_gem cmd do; ruby_version node[:answers][:ruby][:version] end
 end
 
+
+# setup secret files (db.yml, app secrets, etc.)
+puts "Adding template files generated from encrypted data bags:"
+env_config = Chef::EncryptedDataBagItem.load("config", "env")
+
+template '/home/vagrant/answers/.env' do
+	variables env: env_config 
+end
+puts "* generated .env"
+
+puts Dir.pwd
+template '/home/vagrant/answers/config/database.yml' do
+	variables password: db_config['users'][0]['password']
+end
+puts '* generated config/database.yml'
+
+
 # bundle gems, migrate db, reindex elasticsearch
 ['bundle install', 'bundle exec rake db:migrate', 'bundle exec rake searchkick:reindex:all'].each do |cmd|
-	rbenv_execute cmd do; cwd '/home/vagrant/answers' end 
+	rbenv_execute cmd do
+		cwd '/home/vagrant/answers'
+		retries 2
+	end 
 end
