ce660e75662c886f223d4032e6353e8641a5a787
Alan deLevie <adelevie@gmail.com>
2014-08-08 11:20:01 -0400

    cleaned up controllers

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,25 +1,18 @@
-# this will mostly be overwritten by the searchkick pr. just want to ensure tests pass.
-
 class SearchController < ApplicationController
   def index
     query =  params[:q].strip
     return redirect_to root_path if params[:q].blank?
-
-    results = Article.search(query)
-    Rails.logger.info "search-request: IP:#{request.env['REMOTE_ADDR']}, params[:query]:#{query}, QUERY:#{query}, FIRST_RESULT:#{results.first.title unless results.empty?}, RESULTS_N:#{results.size}"
-
-    categories = Category.all
-    if categories.nil?
-      categories = []
-    end
     
+    results = Question.search(query, index_name: [Question.searchkick_index.name, Answer.searchkick_index.name])
+    
+    Rails.logger.info "search-request: IP:#{request.env['REMOTE_ADDR']}, params[:query]:#{query}, QUERY:#{query}, FIRST_RESULT:#{results.first.text unless results.empty?}, RESULTS_N:#{results.size}"
+
     respond_to do |format|
       format.json { render json: results }
       format.html do 
         render locals: {
           results: results, 
-          query: query,
-          categories: categories
+          query: query
         } 
       end
     end
