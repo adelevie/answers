66e90b4bddbcc806913622ab3274a2bd2caa19d0
Alan deLevie <adelevie@gmail.com>
2014-08-26 11:17:00 -0400

    Restored breadcrumbs to Tag pages

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,17 +1,24 @@
 class TagsController < ApplicationController
   caches_page :index
-  caches_page :show  
+  caches_page :show 
+  
+  add_breadcrumb "Home", :root_path
   
   def index
+    add_breadcrumb "Tags", tags_path
+    
     tags = ActsAsTaggableOn::Tag.all
     render locals: {tags: tags}
   end
 
   def show
-    id        = params[:id].to_i
-    tag       = ActsAsTaggableOn::Tag.find(id)
+    id  = params[:id].to_i
+    tag = ActsAsTaggableOn::Tag.find(id)
     
-    taggings  = tag.taggings.select do |tagging| 
+    add_breadcrumb "Tags", tags_path
+    add_breadcrumb tag.name.capitalize, tag_path(tag)
+    
+    taggings = tag.taggings.select do |tagging| 
       tagging.taggable_type == "Question"
     end
     
