cb74294dc28524972479aed0e474c005ec2f6efc
Drew Bailey <drewbailey5@gmail.com>
2014-09-03 17:27:16 -0400

    Quick and basic tag crud operations for admin users

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,8 @@ class Question < ActiveRecord::Base
   has_many :answers
   searchkick wordnet_synonyms: '/var/lib/wn_s.pl'
 
+  attr_writer :tag_ids
+  
   def top_answer
     self.answers.first
   end
