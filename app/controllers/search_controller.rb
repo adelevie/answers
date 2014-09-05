027ef74301200c0cdb2fc4fa17b1399e808daa0d
sashax <alexander.magee@gsa.gov>
2014-09-05 12:02:29 -0700

    lingering site-specific text
    checkpoint for minimal theme. Definitely work in progress

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,11 @@
 class SearchController < ApplicationController
   def index
     query =  params[:q].strip
-    return redirect_to root_path if params[:q].blank?
-    
-    results = Question.search(query, index_name: [Question.searchkick_index.name, Answer.searchkick_index.name])
+    if params[:q].blank?
+       redirect_to root_path
+       return
+    end
+    results = Question.search(query, index_name: [Question.searchkick_index.name])#, Answer.searchkick_index.name])
     
     Rails.logger.info "search-request: IP:#{request.env['REMOTE_ADDR']}, params[:query]:#{query}, QUERY:#{query}, FIRST_RESULT:#{results.first.text unless results.empty?}, RESULTS_N:#{results.size}"
 
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,7 @@
   #for now - will keep themes inside of normal asset pipeline, and attempt to move again later. 
 
   #for Answers/USCIS theme support
-  Rails.application.config.assets.paths << "#{Rails.root}/app/theme/uscis/assets/stylesheets"
+  Rails.application.config.assets.paths << "#{Rails.root}/app/theme/minimal/assets/stylesheets"
   #Rails.application.config.assets.paths << "theme/uscis/stylesheets"  
 
   #Rails.application.config.assets.precompile += %w( theme/uscis/stylesheets/theme.css )  
@@ -12,7 +12,8 @@
   end
 
   #added this to get style-guide to work. Not sure why we need it, but oughtn't do any harm
-  d = Dir.new("#{Rails.root}/app/assets/images/theme/USCIS/")
+  d = Dir.new("#{Rails.root}/app/assets/images/theme/minimal/")
   d.each do |path|
+    Rails.logger.info "adding file #{path}"
     Rails.application.config.assets.precompile << path
   end
