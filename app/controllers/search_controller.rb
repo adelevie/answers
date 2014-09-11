be191abdffa9ee1469d762d341b8675370abd495
Eric Alsheimer <ealsheimer@sequoiainc.com>
2014-09-11 16:00:00 -0400

    Simple non-gem pagination

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,7 @@ class SearchController < ApplicationController
     query =  params[:q].strip
     return redirect_to root_path if params[:q].blank?
     
-    results = Question.search(query, index_name: [Question.searchkick_index.name, Answer.searchkick_index.name])
+    results = Question.search(query, page: params[:page], per_page: 10, index_name: [Question.searchkick_index.name, Answer.searchkick_index.name])
     
     Rails.logger.info "search-request: IP:#{request.env['REMOTE_ADDR']}, params[:query]:#{query}, QUERY:#{query}, FIRST_RESULT:#{results.first.text unless results.empty?}, RESULTS_N:#{results.size}"
 
