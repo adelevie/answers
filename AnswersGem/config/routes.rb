310f35f27509a3d30608c3ab033a90bf88c809bd
Alan deLevie <adelevie@gmail.com>
2014-09-12 18:04:19 -0400

    Transplanted Q&A API controllers + api_controller

--- AnswersGem/config/routes.rb
+++ AnswersGem/config/routes.rb
@@ -2,4 +2,11 @@ AnswersGem::Engine.routes.draw do
   root to: 'home#index'
   
   resources :questions, only: [:index, :show], :path => 'answers', :as => 'answers'
+  
+  namespace :api, defaults: {format: 'json'} do
+    namespace :v1 do
+      resources :questions
+      resources :answers
+    end
+  end
 end
