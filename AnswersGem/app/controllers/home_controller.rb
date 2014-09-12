1c6a69fe5eab1317519ce67a18f88d25c723c160
Alan deLevie <adelevie@gmail.com>
2014-09-12 17:55:50 -0400

    Transplanted home_controller

---
+++ AnswersGem/app/controllers/home_controller.rb
@@ -0,0 +1,18 @@
+module AnswersGem
+  class HomeController < ApplicationController
+
+    caches_page :index
+    add_breadcrumb "Home", :root_url
+
+    def index
+      render locals: { 
+        tags_with_questions: Question.tags_with_questions
+      }
+    end
+
+    def about
+      add_breadcrumb "About"
+    end
+
+  end
+end
--- AnswersGem/config/routes.rb
+++ AnswersGem/config/routes.rb
@@ -1,3 +1,5 @@
 AnswersGem::Engine.routes.draw do  
+  root to: 'home#index'
+  
   resources :questions, only: [:index, :show], :path => 'answers', :as => 'answers'
 end
