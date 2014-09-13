ba3602c68402b8cae6be87cb1fcbec9a9e376e81
sashax <alexander.magee@gsa.gov>
2014-09-12 17:26:13 -0700

    Make what we were calling 'minimal' the base theme

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,9 +1,9 @@
 
   #for now - will keep themes inside of normal asset pipeline, and attempt to move again later. 
 
-  #for Answers/USCIS theme support
-  Rails.application.config.assets.paths << "#{Rails.root}/app/theme/minimal/assets/stylesheets"
-  #Rails.application.config.assets.paths << "theme/uscis/stylesheets"  
+  #for theme support uncomment one of below lines
+  # Rails.application.config.assets.paths << "#{Rails.root}/app/theme/minimal/assets/stylesheets"
+  # Rails.application.config.assets.paths << "theme/uscis/stylesheets"
 
   #Rails.application.config.assets.precompile += %w( theme/uscis/stylesheets/theme.css )  
 
@@ -12,8 +12,8 @@
   end
 
   #added this to get style-guide to work. Not sure why we need it, but oughtn't do any harm
-  d = Dir.new("#{Rails.root}/app/assets/images/theme/minimal/")
-  d.each do |path|
-    Rails.logger.info "adding file #{path}"
-    Rails.application.config.assets.precompile << path
-  end
+  # d = Dir.new("#{Rails.root}/app/assets/images/theme/minimal/")
+  # d.each do |path|
+  #   Rails.logger.info "adding file #{path}"
+  #   Rails.application.config.assets.precompile << path
+  # end
