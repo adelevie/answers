68162c2d5e419ccbabbf7e0ee9956a15ca3e7a23
Alan deLevie <adelevie@gmail.com>
2014-09-15 13:53:30 -0400

    Restoring API controllers and routes.
    - adds the `Answers` namespace to API controllers
    - adds RESTful routes in config/routes.rb to expose these controllers
    - fixes a few small errors in the Tag and Tagging API controllers

---
+++ core/app/controllers/answers/api/v1/taggings_controller.rb
@@ -0,0 +1,46 @@
+module Answers
+  class Api::V1::TaggingsController < Answers::Api::V1::ApiController
+    respond_to :json
+
+    def index
+      taggings = ActsAsTaggableOn::Tagging.all
+      render locals: {taggings: taggings}
+    end
+
+    def show
+      puts "SHOW!!!"
+      tagging = ActsAsTaggableOn::Tagging.find(params[:id])
+      render locals: {tagging: tagging}
+    end
+  
+    def create
+      tagging = ActsAsTaggableOn::Tagging.new(tagging_params)
+      tagging.save
+      render 'tagging/show', locals: {tag: tagging}
+    end
+
+    def update
+      tag = ActsAsTaggableOn::Tagging.find(params[:id])
+      tag.update(tag_params)
+      render 'tagging/show', locals: {tagging: tagging}
+    end
+
+    def destroy
+      tagging = ActsAsTaggableOn::Tagging.find(params[:id])
+      tagging.destroy
+      render 'tagging/show', locals: {tagging: tagging}
+    end
+
+    private
+      def tagging_params
+        params.require(:tagging).permit(
+          :tag_id,
+          :taggable_id,
+          :taggable_type,
+          :tagger_id,
+          :tagger_type,
+          :context
+        )
+      end
+  end
+end
