64cf24508c1678232247b57ac82caa9ad8f6aa89
Alan deLevie <adelevie@gmail.com>
2014-09-23 14:32:47 -0400

    Unpended and fixed the routes_spec.

--- core/spec/routing/routes_spec.rb
+++ core/spec/routing/routes_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
 
-describe "routing to search results", :type => :routing do
-  it "routes /search/ to search#index for a given query" do
-  	pending 'needs to be fixed for new engine layout'
-
-    expect({ :get => "/search/" }).to route_to(
-      :controller => 'search',
-      :action => 'index'
+describe "routing to search results", type: :routing do
+  routes { Answers::Core::Engine.routes }
+  
+  it "routes /search to search#index for a given query" do    
+    expect(get: '/search').to(
+      route_to(controller: 'answers/search', action: 'index')
     )
   end
 
