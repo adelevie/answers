2c0e7c92539b25b8851bad3d001ebd17789051dd
Lindsay Young <lindsayyoung@Lindsays-MacBook-Pro.local>
2014-09-24 17:20:48 -0400

    Revealing user login tests that are failing

--- core/spec/features/admin_dashboard_spec.rb
+++ core/spec/features/admin_dashboard_spec.rb
@@ -8,35 +8,31 @@ describe 'Admin Dashboard', :type => :feature do
       before do
         @article = create(:question, text: 'example title')
         @another_article = create(:question)
-        # login_user
-        # visit admin_dashboard_path
+        login_user
+        visit admin_dashboard_path
       end
 
       it 'displays a list of recent articles' do
-        pending 'needs to be fixed for new engine layout'
-        
+
         expect(page).to have_content('Recent Articles')
         expect(page).to have_content(@article.title)
         expect(page).to have_content(@another_article.title)
       end
 
       it 'displays details about the first article' do
-        pending 'needs to be fixed for new engine layout'
-        
+
         expect(page).to have_content(@article.type)
         expect(page).to have_content(@article.user.email)
       end
 
       it 'displays details about the second article' do
-        pending 'needs to be fixed for new engine layout'
-        
+
         expect(page).to have_content(@another_article.type)
         expect(page).to have_content(@another_article.user.email)
-      end 
+      end
 
       it 'displays the article details page when the article title is clicked' do
-        pending 'needs to be fixed for new engine layout'
-        
+
         click_link @article.title
         expect(page).to have_content(@article.preview)
         expect(page).to have_content(@article.content)
