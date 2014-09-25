239774e69926b12fca375251c6b1069ec5a4a687
LindsayYoung <lindsayn.young@gsa.gov>
2014-09-25 15:05:08 -0400

    add test for about page routing

--- core/spec/routing/routes_spec.rb
+++ core/spec/routing/routes_spec.rb
@@ -2,10 +2,16 @@ require 'spec_helper'
 
 describe "routing to search results", type: :routing do
   routes { Answers::Core::Engine.routes }
-  
-  it "routes /search to search#index for a given query" do    
+
+  it "routes /search to search#index for a given query" do
     expect(get: '/search').to(
       route_to(controller: 'answers/search', action: 'index')
+    )
+  end
+
+  it "routes /about to home#about" do
+    expect(get: '/about').to(
+      route_to(controller: 'answers/home', action: 'about')
     )
   end
 
