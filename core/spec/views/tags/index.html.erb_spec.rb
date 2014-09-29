82ae9adcb92fd8ccad5b3a0f5f81eb73bfa1e9cb
LindsayYoung <lindsayn.young@gsa.gov>
2014-09-29 13:19:10 -0400

    Added a tag test where there was a pending test placeholder.
    114 examples, 0 failures, 10 pending

--- core/spec/views/tags/index.html.erb_spec.rb
+++ core/spec/views/tags/index.html.erb_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 RSpec.describe "tags/index.html.erb", :type => :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    assign(:question,[
+      Answers::Question.create!()
+    ])
+  end
+
+  it "renders tags" do
+    controller do
+      render
+    end
+  end
+
 end
