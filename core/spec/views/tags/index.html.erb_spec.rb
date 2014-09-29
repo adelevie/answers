48fda3bd90c9609c35dafce65a1b8af026e679f2
LindsayYoung <lindsayn.young@gsa.gov>
2014-09-29 13:57:52 -0400

    Ensures multiple tags exist for testing tags.

--- core/spec/views/tags/index.html.erb_spec.rb
+++ core/spec/views/tags/index.html.erb_spec.rb
@@ -2,9 +2,19 @@ require 'spec_helper'
 
 RSpec.describe "tags/index.html.erb", :type => :view do
   before(:each) do
-    assign(:question,[
-      Answers::Question.create!()
-    ])
+    words = %w[
+      pets cars education transportation business employment housing trash
+    ]
+
+    tags = words.map do |word|
+      create(:tag, name: word)
+    end
+
+    100.times do
+      q = create(:question)
+      q.tag_list = [tags.sample]
+      q.save
+    end
   end
 
   it "renders tags" do
