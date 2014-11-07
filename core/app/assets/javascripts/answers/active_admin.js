f8978d103fd957c113a7ba2c7dfed03663b251aa
Drew Bailey <drewbailey5@gmail.com>
2014-11-06 19:42:53 -0500

    commenting out active admin js & css

--- core/app/assets/javascripts/answers/active_admin.js
+++ core/app/assets/javascripts/answers/active_admin.js
@@ -1,24 +1,24 @@
-//= require active_admin/base
-//= require ./pagedown/Markdown.Converter.js
-//= require ./pagedown/Markdown.Editor.js
-//= require ./pagedown/Markdown.Sanitizer.js
-//= require ./Markdown.Extra.js
+// require active_admin/base
+// require ./pagedown/Markdown.Converter.js
+// require ./pagedown/Markdown.Editor.js
+// require ./pagedown/Markdown.Sanitizer.js
+// require ./Markdown.Extra.js
 
-$(document).ready(function(){
-	// Clicking Articles link on dashboard > open up context menu
-	$("a[href $= '/admin/articles']").attr("href","#");
-	$("a[href $= '#']").click(function(e){
-		$("a[href $= '#']").next().css({ "display": "block" });
-	});
+// $(document).ready(function(){
+// 	// Clicking Articles link on dashboard > open up context menu
+// 	$("a[href $= '/admin/articles']").attr("href","#");
+// 	$("a[href $= '#']").click(function(e){
+// 		$("a[href $= '#']").next().css({ "display": "block" });
+// 	});
 
-	var arrows = $(".guidearrow");
-	if(arrows.length){
-		setInterval(function(){
-			for(var a=0;a<arrows.length;a++){
-				$(arrows[a]).css({ "display": "inline" });
-			}
-		}, 250);
-	}
+// 	var arrows = $(".guidearrow");
+// 	if(arrows.length){
+// 		setInterval(function(){
+// 			for(var a=0;a<arrows.length;a++){
+// 				$(arrows[a]).css({ "display": "inline" });
+// 			}
+// 		}, 250);
+// 	}
 
 	/* if( $("#page_title").text() == "Dashboard"){
 		var headers = $("thead th");
