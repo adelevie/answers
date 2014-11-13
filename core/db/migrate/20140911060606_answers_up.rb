66957391338721697bc5678cff559f431c9d8a9b
Drew Bailey <drewbailey5@gmail.com>
2014-11-13 09:27:49 -0500

    check if table exists before running migrations that could have been already installed

--- core/db/migrate/20140911060606_answers_up.rb
+++ core/db/migrate/20140911060606_answers_up.rb
@@ -2,77 +2,82 @@ class AnswersUp < ActiveRecord::Migration
 
   def change
 
-    create_table :answers_answers do |t|
-      t.text :need_to_know
-      t.text :text
-      t.string :url
-      t.string :in_language
-      t.references :question, index: true
+    unless ActiveRecord::Base.connection.table_exists?('answers_answers')
+      create_table :answers_answers do |t|
+        t.text :need_to_know
+        t.text :text
+        t.string :url
+        t.string :in_language
+        t.references :question, index: true
 
-      t.timestamps
+        t.timestamps
+      end
+
+
+      add_index :answers_answers, :id
     end
 
-    add_index :answers_answers, :id
+    unless ActiveRecord::Base.connection.table_exists?('answers_questions')
+      create_table :answers_questions do |t|
+        t.string :text
+        t.string :url
+        t.string :in_language
 
+        t.timestamps
+      end
 
-    create_table :answers_questions do |t|
-      t.string :text
-      t.string :url
-      t.string :in_language
-
-      t.timestamps
+      add_index :answers_questions, :id
     end
-
-    add_index :answers_questions, :id
 
 
     # USERS
+    unless ActiveRecord::Base.connection.table_exists?('answers_users')
+      create_table(:answers_users) do |t|
+        ## Database authenticatable
+        t.string :email,              :null => false, :default => ""
+        t.string :encrypted_password, :null => false, :default => ""
 
-    create_table(:answers_users) do |t|
-      ## Database authenticatable
-      t.string :email,              :null => false, :default => ""
-      t.string :encrypted_password, :null => false, :default => ""
+        ## Recoverable
+        t.string   :reset_password_token
+        t.datetime :reset_password_sent_at
 
-      ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+        ## Rememberable
+        t.datetime :remember_created_at
 
-      ## Rememberable
-      t.datetime :remember_created_at
+        ## Trackable
+        t.integer  :sign_in_count, :default => 0
+        t.datetime :current_sign_in_at
+        t.datetime :last_sign_in_at
+        t.string   :current_sign_in_ip
+        t.string   :last_sign_in_ip
 
-      ## Trackable
-      t.integer  :sign_in_count, :default => 0
-      t.datetime :current_sign_in_at
-      t.datetime :last_sign_in_at
-      t.string   :current_sign_in_ip
-      t.string   :last_sign_in_ip
+        ## Encryptable
+        # t.string :password_salt
 
-      ## Encryptable
-      # t.string :password_salt
+        ## Confirmable
+        # t.string   :confirmation_token
+        # t.datetime :confirmed_at
+        # t.datetime :confirmation_sent_at
+        # t.string   :unconfirmed_email # Only if using reconfirmable
 
-      ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+        ## Lockable
+        # t.integer  :failed_attempts, :default => 0 # Only if lock strategy is :failed_attempts
+        # t.string   :unlock_token # Only if unlock strategy is :email or :both
+        # t.datetime :locked_at
 
-      ## Lockable
-      # t.integer  :failed_attempts, :default => 0 # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      # t.datetime :locked_at
+        # Token authenticatable
+        t.string :authentication_token
 
-      # Token authenticatable
-      t.string :authentication_token
+        ## Invitable
+        # t.string :invitation_token
 
-      ## Invitable
-      # t.string :invitation_token
+        t.timestamps
 
-      t.timestamps
-
-      # 
-      t.boolean  "is_editor"
-      t.boolean  "is_admin",               default: false
-      t.boolean  "is_writer",              default: false
+        # 
+        t.boolean  "is_editor"
+        t.boolean  "is_admin",               default: false
+        t.boolean  "is_writer",              default: false
+      end
     end
   end
 
